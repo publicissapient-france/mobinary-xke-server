d15bc6faaf268be3b2f8d6134dca072af1bd5970
Souhaib <ssguitouni@gmail.com>
2018-03-03 19:49:19 +0100

    [ADD] add avro node

--- src/main/kotlin/fr/xebia/Main.kt
+++ src/main/kotlin/fr/xebia/Main.kt
@@ -1,11 +1,13 @@
 package fr.xebia
 
+import example.avro.Game
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.CallLogging
 import io.ktor.features.DefaultHeaders
 import io.ktor.response.respond
+import io.ktor.response.respondText
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 
@@ -14,14 +16,23 @@ fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
     install(Routing) {
-        get("/") {
-            val game = HeroOuterClass.Hero.newBuilder()
+        get("/hero") {
+            call.respondText("Get a Game or a hero")
+        }
+
+        get("/hero") {
+            val hero = HeroOuterClass.Hero.newBuilder()
                     .setId(666)
                     .setName("Eric Cartman")
                     .setDescription("Whateva, I do what I want!")
                     .build()
+            call.respond(hero.toByteArray())
+        }
 
-            call.respond(game.toByteArray())
+        get("/game") {
+            val game = Game("Game name", "Game desciption");
+
+            call.respond(game.toByteBuffer())
         }
     }
 }
